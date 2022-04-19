72918641fcb6fc4b4ff309e06dbf920e0f38adcb
ahmadfareed <a.fareed@pioneerlogics.com>
2022-04-19 15:03:38 +0500

    added modal functionality

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ const movieRow = document.querySelector(".movie__row");
 const moviesContainer = document.querySelector(".container");
 const searchEl = document.querySelector(".search__form--input");
 const btn = document.querySelector(".btn");
-let btnDetailEventAdd = false;
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
 const renderMovies = function (movie, i) {
   const html = `
 <div class="movie__row">
-<img class="thumbnail" src=${movie.Poster}></img>
+<img class="thumbnail" src=${movie.Poster} alt="not available"></img>
 <div class="movie__details">
     <div class="detail name${i}">
         <label>Name:</label> ${movie.Title}
@@ -36,15 +37,31 @@ const renderMovies = function (movie, i) {
   // event handler to render single movie on click
   btnDetail.addEventListener("click", function () {
     const mName = movieName.textContent.trim().split(" ").splice(1).join(" ");
-    moviesContainer.innerHTML = "";
+    // moviesContainer.innerHTML = "";
 
     getMovieDetails(mName);
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
 };
 
 const renderSingleMovie = function (movie) {
   const html = `
 <div class="single__movie">
+<svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 close__modal"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
 <img class="single__movie__img" src=${movie.Poster} alt="" />
 <div class="single__movie__details">
   <div class="single__name">
@@ -68,8 +85,8 @@ const renderSingleMovie = function (movie) {
 </div>
 </div>
 `;
-  moviesContainer.innerHTML = "";
-  moviesContainer.insertAdjacentHTML("beforeend", html);
+  // modal.innerHTML = "";
+  modal.insertAdjacentHTML("afterbegin", html);
 };
 
 const getJson = function (url) {
@@ -84,6 +101,9 @@ const getJson = function (url) {
 
 const myKey = "3167af42";
 
+/////////////////// API requesting Funcs ///////////////////
+
+///////////////////// getting movies list ////////////////////////
 const getMoviesByName = function (name) {
   const url = `http://www.omdbapi.com/?s=${name}&apikey=${myKey}&r=json`;
   //   const url = `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${name}`;
@@ -91,38 +111,40 @@ const getMoviesByName = function (name) {
   getJson(url).then(function (data) {
     console.log(data);
     const arr = data.Search;
-    console.log(arr);
+    // console.log(arr);
     arr.forEach(function (el, i) {
       renderMovies(el, i);
     });
   });
 };
-
+///////////////////////// get one movie ///////////////////////////
 const getMovieDetails = function (name) {
   const url = `http://www.omdbapi.com/?t=${name}&apikey=${myKey}`;
 
   getJson(url).then(function (details) {
     // console.log(details);
     renderSingleMovie(details);
+
+    // close model element is rendered after movie details button
+    // is click hence it does ot exist prior to this
+    // so we need to make selection here
+    const closeModal = document.querySelector(".close__modal");
+    closeModal.addEventListener("click", function () {
+      modal.classList.add("hidden");
+      overlay.classList.add("hidden");
+
+      modal.innerHTML = "";
+    });
   });
 };
 
-// event handlers
+//////////////////////// event handlers ///////////////////////////
 
 btn.addEventListener("click", function (e) {
   e.preventDefault();
-  const movieName = searchEl.value;
+  // const movieName = searchEl.value;
 
   moviesContainer.innerHTML = "";
-  console.log(movieName);
-  getMoviesByName(movieName);
+  // console.log(movieName);
+  getMoviesByName(searchEl.value);
 });
-
-// btnDetail.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   moviesContainer.innerHTML = "";
-//   getMovieDetails(movieName.value);
-// });
-
-// getMoviesByName('batman');
-// renderMovies();
